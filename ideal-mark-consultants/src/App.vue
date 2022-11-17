cbf0414f6b2b28d880ff3f61e1c13d21613bef54
pundi-creator <78798818+pundi-creator@users.noreply.github.com>
2022-11-17 11:27:38 +0300

    initial commit

---
+++ ideal-mark-consultants/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import {RouterLink, RouterView} from 'vue-router'
+</script>
+
+<template>
+  <img class="h-full w-full m-0 p-0 object-cover absolute bg-cover" src="./assets/WhatsApp Image 2022-09-30 at 10.11 1.png" alt="">
+<div class="flex py-20 bg-transparent p-0 m-0 box-border">
+<nav class="relative p-4 md:flex md:justify-end md:items-end md:space-x-10 text-white border-white border-2 w-full border-r-0 border-l-0 pr-10">
+        <RouterLink class="flex gap-10" to="/"><img src="./assets/IdealMark 2.png" alt=""></RouterLink>
+       <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About Us</RouterLink>
+        <RouterLink to="/services">Services</RouterLink>
+        <RouterLink to="/contact">Contact us</RouterLink>
+</nav>
+</div>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ ideal-mark-consultants/src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Contact from '../views/Contact.vue'
+import Services from '../views/Services.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/About.vue')
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact
+    },
+    {
+      path: '/services',
+      name: 'services',
+      component: Services
+    }
+  ]
+})
+
+export default router
